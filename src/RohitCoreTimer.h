672ec2879631ab1ca9690314b681db7dbe3699a3
rohitmaji004 <153757813+rohitmaji004@users.noreply.github.com>
2026-03-26 15:43:52 +0530

    Update RohitCoreTimer.h

--- src/RohitCoreTimer.h
+++ src/RohitCoreTimer.h
@@ -1,1 +1,31 @@
+#ifndef RohitCoreTimer_h
+#define RohitCoreTimer_h
 
+#include <Arduino.h>
+#include <RohitCoreBase.h>
+
+#define MAX_TASKS 10
+
+typedef void (*TaskCallback)();
+
+struct Task {
+  unsigned long interval;
+  unsigned long lastRun;
+  TaskCallback callback;
+  bool repeat;
+  bool active;
+};
+
+class RohitCoreTimer {
+  public:
+    RohitCoreTimer();
+
+    void every(unsigned long interval, TaskCallback callback);
+    void after(unsigned long delay, TaskCallback callback);
+    void update();
+
+  private:
+    Task tasks[MAX_TASKS];
+};
+
+#endif
